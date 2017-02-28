9277d901a70e0a7d9dc5ff0d3bdbb9f704bd97d2
Neil Devine <nm31devi@siena.edu>
2017-02-28 17:31:50 -0500

    Add files via upload

---
+++ Hello World/MainClass.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class MainClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MainClass
+{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
